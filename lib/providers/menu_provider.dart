0e8510239e833176f4c885d7a4c3aa99b4f37f92
Emmanuel Correa <emmanuelcorrea@MacBook-Pro-de-Informatica.local>
2019-10-08 17:25:27 -0300

    imprimendo Json por consola

--- lib/providers/menu_provider.dart
+++ lib/providers/menu_provider.dart
@@ -1,14 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart' show rootBundle;
 
 
-class MenuProvider{
+class _MenuProvider{
 
   List<dynamic> opciones = [];
-  MenuProvider(){
+  _MenuProvider(){
     cargarData();
   }
 
-  cargarData(){
-    rootBundle.loadString('data/menu_opt.json');
+  cargarData(){ 
+    rootBundle.loadString('data/menu_opts.json')
+    .then( (data) {
+        //Map dataMap = json.decode(data);
+      
+        print(data);
+
+    });
   }
 }
+
+final menuProvider = new _MenuProvider(); //estoi ejecuta las variable privadas
