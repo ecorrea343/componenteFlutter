f06cc570c3bf343085438d3f9b17c9bed5663a3a
Emmanuel Correa <ebcorreac@gmail.com>
2019-10-11 12:57:40 -0300

    terminando el Widget Avatar  video 77

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -2,17 +2,44 @@ import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
 
-    
     @override
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(
 
             title: Text('Avatar Page'),
+            /*
+            *Cambios realizados en el video 77 de widget con el elemente Circle Avatar
+             */
+            actions: <Widget>[
+             
+             Container(
+               padding:EdgeInsets.all(5.0) ,
+              child :CircleAvatar(
+              backgroundImage: NetworkImage('https://img.vixdata.io/pd/jpg-large/es/sites/default/files/r/rip-stan-lee.jpg') ,
+              radius: 22.0,
 
+            ),
+            ),
+
+
+             Container(
+                margin: EdgeInsets.only(right: 10.0),
+                child: CircleAvatar( 
+                  child:Text('SL'),
+                  backgroundColor: Colors.brown,
+                )
+              ),
+            ],
         ),
 
+        body: Center(
+          child: FadeInImage(
+            image: NetworkImage('https://img.vixdata.io/pd/jpg-large/es/sites/default/files/r/rip-stan-lee.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+          ),
+        ),
       ) ;
     }
-
 }
