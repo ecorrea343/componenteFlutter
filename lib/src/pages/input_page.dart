a334a4a3d04f393fda1ec1c25b1a5d2a4c37d58b
Emmanuel Correa <ebcorreac@gmail.com>
2019-10-11 16:14:02 -0300

    Email Y password Fields

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -7,7 +7,9 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
 
-  String _nombre = '';
+  String _nombre    = '';
+  String _email     = '';
+  String _password  = ''; 
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +21,10 @@ class _InputPageState extends State<InputPage> {
         padding: EdgeInsets.symmetric(horizontal: 10.0 , vertical: 20.0),
         children: <Widget>[
           _crearInput(),
+          Divider(),
+          _crearEmail(), //Modificaciones hecahs en el video 80
+          Divider(),
+          _crearPassword(),
           Divider(),
           _crearPersona(), //Modificaciones hechas en el video 79
                   ],
@@ -59,6 +65,53 @@ class _InputPageState extends State<InputPage> {
     Widget _crearPersona (){
         return ListTile(
             title: Text('Nombre es: $_nombre'),
+            subtitle: Text('Email : $_email'),
+            
+        );
+    }
+
+    //metodo CREADO en el video numero 80
+    Widget _crearEmail(){
+      
+      return TextField(
+          //autofocus: true,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(
+
+                borderRadius: BorderRadius.circular(20.0),
+
+              ),
+             // counter: Text('Letra ${_nombre.length}'),
+              hintText: 'Email',
+              labelText: 'Email',
+             // helperText: 'Solo es el nombre ',
+              suffixIcon: Icon(Icons.alternate_email),
+              icon: Icon(Icons.email)
+          ),
+          onChanged: (valor) => setState(() {  _email = valor; })  
+         
+        );
+    }
+
+    Widget _crearPassword(){
+      return TextField(
+          //autofocus: true,
+          obscureText: true,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(
+
+                borderRadius: BorderRadius.circular(20.0),
+
+              ),
+              //counter: Text('Letra ${_nombre.length}'),
+              hintText: 'Password',
+              labelText: 'Password',
+              //helperText: 'Solo es el nombre ',
+              suffixIcon: Icon(Icons.lock_open),
+              icon: Icon(Icons.lock)
+          ),
+            onChanged: (valor) => setState(() {  _password = valor; }) 
         );
     }
 }
